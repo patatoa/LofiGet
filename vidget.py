3531b9951f94f08339a81780eb96035b49c4ce86
patatoa <patatoa@gmail.com>
2023-02-04 12:19:21 -0600

    Cleanup (#1)
    * code cleanup
    
    * Update readme

--- vidget.py
+++ vidget.py
@@ -4,12 +4,11 @@ import os
 from vidgear.gears import CamGear
 
 def vidget() -> str:
-	stream = CamGear(source='https://www.youtube.com/watch?v=jfKfPfyJRdk', stream_mode =True,  time_delay=1, logging=True).start()
+	ytUrl = os.environ['ytUrl']
+	stream = CamGear(source=ytUrl, stream_mode =True,  time_delay=1, logging=True).start()
 
 	path = "."
 
-	currentframe = 0
-	# Get the current datetimestamp as string
 	timestamp = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
 
 	frame = stream.read() 
@@ -17,7 +16,6 @@ def vidget() -> str:
 		raise ValueError("No frame read from stream")
 
 	name = path + '/frame' + timestamp + '.jpg'
-	print ('Creating...' + name) 
 
 	cv2.imwrite(name, frame)
 
