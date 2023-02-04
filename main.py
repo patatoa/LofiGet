234b6fe2ff8123b02b2f0becaa9eca6f8953f0ef
patatoa <patatoa@gmail.com>
2023-02-03 22:37:31 -0600

    actually cloud run this

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 def saveToGCPBucket(path, bucketName) -> None:
 	from google.cloud import storage
 	import os
@@ -6,11 +10,18 @@ def saveToGCPBucket(path, bucketName) -> None:
 	blob = bucket.blob(os.path.basename(path))
 	blob.upload_from_filename(path)
 
+@app.get("/")
 def main() -> None:
 	import vidget
 	path = vidget.vidget()
 	bucketName = "lofigirlframes"
 	saveToGCPBucket(path, bucketName)
+	return f"Saved {path} to {bucketName}"
 
 if __name__ == "__main__":
-	main()
+    # Development only: run "python main.py" and open http://localhost:8080
+    # When deploying to Cloud Run, a production-grade WSGI HTTP server,
+    # such as Gunicorn, will serve the app.
+    app.run(host="localhost", port=8080, debug=True)
+
+
