bd6aa4462d20cdf7046b606a33f18d66e12505b6
patatoa <patatoa@gmail.com>
2023-02-05 22:46:20 -0600

    start recording to db

---
+++ lofiget/firestoreDataAccess.py
@@ -0,0 +1,72 @@
+from collections import namedtuple
+from datetime import datetime
+from google.cloud import firestore
+
+FirestoreCollectionNames = namedtuple('FirestoreCollectionNames', ['frames', 'currentSky', 'skyDurations'])
+
+class FirestoreCollections:
+    __instance = None
+
+    @staticmethod 
+    def setInstance(collectionNames: FirestoreCollectionNames):
+        if FirestoreCollections.__instance == None:
+            FirestoreCollections(collectionNames)
+        return FirestoreCollections.__instance
+
+    @staticmethod 
+    def getInstance():
+        if FirestoreCollections.__instance == None:
+            raise Exception("FirestoreCollectionNames has not been initialized")
+        return FirestoreCollections.__instance
+
+    def __init__(self, collectionNames: FirestoreCollectionNames):
+        if FirestoreCollections.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            FirestoreCollections.__instance = self
+            self.env_vars = {}
+            if collectionNames == None:
+                raise Exception("collectionNames cannot be None")
+            else:
+                self.frames = collectionNames.frames
+                self.currentSky = collectionNames.currentSky
+                self.skyDurations = collectionNames.skyDurations
+
+def saveToFirestore(data: dict) -> None:
+	db = firestore.Client()
+	db.collection(FirestoreCollections.getInstance().frames).add(data)
+
+def checkForSkyChange(currentSnapshot: dict) -> bool:
+	def getCurrentSkyQuery() -> firestore.Query:
+		return db.collection(FirestoreCollections.getInstance().currentSky).limit(1).get()[0]
+
+	def getCurrentSkyData() -> dict:
+		return getCurrentSkyQuery().to_dict()
+
+	def updateCurrentSkyData(data: dict) -> None:
+		currentSky = getCurrentSkyQuery()
+		currentSky.reference.update({
+                      'timeStart': data['datetime'],
+                      'url': data['url'],
+                      'sky': data['sky'],
+                      'isApproximateStart': False
+		})
+
+	def updateSkyDurations(data: dict, currentTime: datetime) -> None:
+		recentSkyDuration = db.collection(FirestoreCollections.getInstance().skyDurations).where('timeEnd', '==', '').get()
+		print(recentSkyDuration.__len__())
+		for key in recentSkyDuration:
+			key.reference.update({'timeEnd': currentTime})
+		db.collection(FirestoreCollections.getInstance().skyDurations).add({'timeStart': currentTime, 'sky': data['sky'], 'timeEnd': ""})
+
+	db = firestore.Client()
+	currentSkyData = getCurrentSkyData()
+	print(currentSkyData['sky'])
+	if currentSkyData['sky'] == currentSnapshot['sky']:
+		return False
+	else:
+		updateCurrentSkyData(currentSnapshot)
+		print("Sky changed to " + currentSnapshot['sky'])
+		updateSkyDurations(currentSnapshot, datetime.now())
+		print("Sky duration updated")
+		return True
--- lofiget/main.py
+++ lofiget/main.py
@@ -1,31 +1,45 @@
+from datetime import datetime
+from firestoreDataAccess import saveToFirestore, checkForSkyChange, FirestoreCollections, FirestoreCollectionNames
 from flask import Flask
 import os
 
+from skyCheck import getSkyData
+
 app = Flask(__name__)
 
+
 def saveToGCPBucket(path, bucketName) -> str:
-	from google.cloud import storage
-	storage_client = storage.Client()
-	bucket = storage_client.get_bucket(bucketName)
-	blob = bucket.blob(os.path.basename(path))
-	blob.upload_from_filename(path)
-	return blob.public_url
+    from google.cloud import storage
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucketName)
+    blob = bucket.blob(os.path.basename(path))
+    blob.upload_from_filename(path)
+    return blob.public_url
+
 
 def getBucketName() -> str:
-	return os.environ.get('lofigirlframesbucket', 'lofigirlframes')
+    return os.environ.get('lofigirlframesbucket', 'lofigirlframes')
+
 
 @app.get("/")
 def main() -> None:
-	from vidget import vidget
-	path = vidget()
-	bucketName = getBucketName()
-	finalUrl = saveToGCPBucket(path, bucketName)
-	return finalUrl
+    from vidget import vidget
+    currentDateTime = datetime.now()
+    path = vidget()
+    bucketName = getBucketName()
+    finalUrl = saveToGCPBucket(path, bucketName)
+    skyData = getSkyData(path)
+    FirestoreCollections.setInstance(FirestoreCollectionNames(
+        os.environ['framesCollection'], os.environ['currentSkyCollection'], os.environ['skyDurationsCollection']))
+    currentSkyData = {'datetime': currentDateTime, 'url': finalUrl,
+                      'sky': skyData.sky, 'brightnessDelta': skyData.brightnessDelta}
+    saveToFirestore(currentSkyData)
+    checkForSkyChange(currentSkyData)
+    return currentSkyData
+
 
 if __name__ == "__main__":
     # Development only: run "python main.py" and open http://localhost:8080
     # When deploying to Cloud Run, a production-grade WSGI HTTP server,
     # such as Gunicorn, will serve the app.
     app.run(host="localhost", port=8080, debug=True)
-
-
