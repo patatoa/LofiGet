26d5a71e00c18ec714da34f8c0abee60cff68b92
patatoa <patatoa@gmail.com>
2023-02-07 12:49:38 -0600

    move download function

--- lofiget/skyCheck.py
+++ lofiget/skyCheck.py
@@ -6,10 +6,16 @@ from collections import namedtuple
 SkyData = namedtuple('SkyData', ['sky', 'brightnessDelta'])
 
 
-def getSkyData(img) -> SkyData:
+def getSkyData(url) -> SkyData:
+	def download_image(url):
+		response = urllib.request.urlopen(url)
+		img_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
+		return cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
+
 	def mean_brightness(img):
 		return np.mean(img)
 
+	img = download_image(url)
 	brightness = mean_brightness(img)
 	threshold = 60
 	if brightness < threshold:
@@ -20,15 +26,10 @@ def getSkyData(img) -> SkyData:
 	return SkyData(sky, brightnessDelta)
 
 def main():
-	def download_image(url):
-		response = urllib.request.urlopen(url)
-		img_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
-		return cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
 
 	url = "https://www.ft.com/__origami/service/image/v2/images/raw/https://d1e00ek4ebabms.cloudfront.net/production/5e0b4328-ff30-4ed3-a2e2-7feaebda9fa0.png?source=next&fit=scale-down&quality=highest&width=720&dpr=2"
 	# url = "https://storage.googleapis.com/lofigirlframes/frame06-Feb-2023%20(02%3A00%3A09.387373).jpg"
-	img = download_image(url)
-	if getSkyData(img).sky == 'night':
+	if getSkyData(url).sky == 'night':
 		print("The image is a night scene.")
 	else:
 		print("The image is a day scene.")
