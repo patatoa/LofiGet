06bd6a64ee4eaff9e5ee3f7d02f69bb2f20f5ae9
patatoa <patatoa@gmail.com>
2026-04-16 17:28:59 -0500

    -

--- lofiget/vidget.py
+++ lofiget/vidget.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 import cv2
 import os
+import yt_dlp
 from vidgear.gears import CamGear
 
 def vidget():
 	ytUrl = os.environ['ytUrl']
-	stream = CamGear(source=ytUrl, stream_mode=True, time_delay=1, logging=True,
-		**{"yt_dlp_options": {"cookiefile": "/secrets/yt-cookies.txt"}}).start()
+	ydl_opts = {
+		'cookiefile': '/secrets/yt-cookies.txt',
+		'format': 'best[ext=mp4]/best',
+		'quiet': True,
+	}
+	with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+		info = ydl.extract_info(ytUrl, download=False)
+		stream_url = info.get('url') or info['formats'][-1]['url']
+	stream = CamGear(source=stream_url, logging=True).start()
 
 	path = "."
 
